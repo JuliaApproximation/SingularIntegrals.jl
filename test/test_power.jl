a868f796e3284988ed3adec8c21511ab3dcc1f05
Sheehan Olver <solver@mac.com>
2023-04-05 11:41:15 +0100

    add inside interval moments (#6)
    * add inside interval moments
    
    * Update power.jl
    
    * don't use resize!
    
    * Update test_recurrence.jl

--- test/test_power.jl
+++ test/test_power.jl
@@ -12,22 +12,34 @@ using SingularIntegrals: PowerKernelPoint, powerlawmoment, powerlawrecurrence, R
 
 
 @testset "Weights" begin
-    z = 10.0
-    x = axes(ChebyshevT(),1)
-    α = 0.1
-    L = abs.(z .- x') .^ α
-    # from Mathmatica
-    λ = 1
-    L0 = powerlawmoment(Val(0), α, λ, z)
-    L1 = powerlawmoment(Val(1), α, λ, z)
-    @test L* UltrasphericalWeight(λ) ≈ L0  ≈ 1.9772924292721128
-    @test L1 ≈ -0.009901716900034385
-    A, B, C = powerlawrecurrence(α, λ)
-    @test (A[2]z + B[2])*L1-C[2]L0 ≈ -0.00022324029766696007
-    r = RecurrenceArray(z, (A,B,C), [L0,L1])
-    @test r[5] ≈ -2.5742591209035326E-7 
-    @test r[1:10] ≈ (L * Weighted(Ultraspherical(λ)))[1,1:10]
+    @testset "on interval" begin
+        z = 0.2
+        x = axes(ChebyshevT(),1)
+        α = 0.1
+        L = abs.(z .- x') .^ α
+        λ = 1
+        L0 = powerlawmoment(Val(0), α, λ, z)
+        # L1 = powerlawmoment(Val(1), α, λ, z)
 
-    @test L* LegendreWeight() ≈ 2.517472100701719
-    @test (L * Legendre())[5] ≈ -1.3328397976790363E-7 
+        @test L* UltrasphericalWeight(λ) ≈ L0  ≈ 1.407056672015012 # from Mathmatica
+    end
+    @testset "off interval" begin
+        z = 10.0
+        x = axes(ChebyshevT(),1)
+        α = 0.1
+        L = abs.(z .- x') .^ α
+        λ = 1
+        L0 = powerlawmoment(Val(0), α, λ, z)
+        L1 = powerlawmoment(Val(1), α, λ, z)
+        @test L* UltrasphericalWeight(λ) ≈ L0  ≈ 1.9772924292721128 # from Mathmatica
+        @test L1 ≈ -0.009901716900034385
+        A, B, C = powerlawrecurrence(α, λ)
+        @test (A[2]z + B[2])*L1-C[2]L0 ≈ -0.00022324029766696007
+        r = RecurrenceArray(z, (A,B,C), [L0,L1])
+        @test r[5] ≈ -2.5742591209035326E-7 
+        @test r[1:10] ≈ (L * Weighted(Ultraspherical(λ)))[1,1:10]
+
+        @test L* LegendreWeight() ≈ 2.517472100701719
+        @test (L * Legendre())[5] ≈ -1.3328397976790363E-7 
+    end
 end
