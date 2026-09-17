812495770dbb34a2164f7b099ee16e3179b3ea87
Sheehan Olver <solver@mac.com>
2026-09-17 14:08:11 +0100

    Update SingularIntegrals.jl

--- src/SingularIntegrals.jl
+++ src/SingularIntegrals.jl
@@ -20,7 +20,7 @@ include("power.jl")
 
 
 ### generic fallback
-for Op in (:Hilbert, :StieltjesPoint, :LogKernelPoint, :PowerKernelPoint, :LogKernel)
+for Op in (:PVStieltjes, :Stieltjes, :StieltjesPoint, :LogKernelPoint, :PowerKernelPoint, :LogKernel)
     @eval begin
         @simplify function *(H::$Op, wP::WeightedBasis{<:Any,<:Weight,<:Any})
             w,P = wP.args
